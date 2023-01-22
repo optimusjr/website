04d65bc4c465b1d27eca6e162d46bfbdc3676553
Silas Henrique <silash35@gmail.com>
2023-01-22 19:24:00 -0300

    testing new layouts

--- src/components/pages/Differentials/icons/Book.tsx
+++ src/components/pages/Differentials/icons/Book.tsx
@@ -1,30 +1,8 @@
-import { m } from "framer-motion";
-
-const icon = {
-  hidden: {
-    scale: 0,
-    rotate: 180,
-  },
-  visible: {
-    scale: 1,
-    rotate: 0,
-  },
-};
-
 const BookIcon = () => {
   return (
-    <m.svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      initial="hidden"
-      whileInView="visible"
-      viewport={{ once: true }}
-    >
-      <m.path
-        variants={icon}
-        d="M12,3L1,9L12,15L21,10.09V17H23V9M5,13.18V17.18L12,21L19,17.18V13.18L12,17L5,13.18Z"
-      />
-    </m.svg>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <path d="M12,3L1,9L12,15L21,10.09V17H23V9M5,13.18V17.18L12,21L19,17.18V13.18L12,17L5,13.18Z" />
+    </svg>
   );
 };
 
--- src/components/pages/Differentials/index.tsx
+++ src/components/pages/Differentials/index.tsx
@@ -1,71 +1,48 @@
-import { useState } from "react";
-
 import styles from "./differentials.module.scss";
 import CapIcon from "./icons/Book";
 import CheckIcon from "./icons/Check";
 import HandIcon from "./icons/Hand";
-// import LightbulbIcon from "./icons/Lightbulb";
-// import TargetIcon from "./icons/Target";
 import TenIcon from "./icons/Ten";
 
-const differentialsArray = [
-  {
-    title: "Mais de 10 anos no mercado",
-    subtitle: "Impactando vidas e negócios",
-    Icon: <TenIcon />,
-    text: "A OPTIMUS Jr. foi fundada em bla bla bla bla bla",
-  },
-  {
-    title: "Melhor preço do mercado",
-    subtitle: "Preços baixos com alta qualidade de serviço",
-    Icon: <HandIcon />,
-    text: "A OPTIMUS Jr. é uma empresa junior, e portanto é sem fins lucrativos. O que faz dos nossos serviços bla bla bla bla",
-  },
-  {
-    title: "Vasto conhecimento técnico",
-    subtitle: "Apoio dos melhores professores da UFBA",
-    Icon: <CapIcon />,
-    text: "A OPTIMUS Jr. é a empresa junior do curso de Engenharia de Controle e Automação da UFBA e bla bla bla",
-  },
-  {
-    title: "Selo de qualidade",
-    subtitle: "Quem contrata, recomenda. A nossa média de NPS é 9,5.",
-    Icon: <CheckIcon />,
-    text: "NPS é bla bla bla bla bla",
-  },
-];
-
 const Differentials = () => {
-  const [selectedDifferential, setSelectedDifferential] = useState(1);
-
   return (
     <article className={styles.differentials}>
-      <div className={styles.title}>
-        <span>Diferenciais</span>
-        <h2>Por que somos a escolha certa?</h2>
-      </div>
+      <span>Diferenciais</span>
+      <h2>Por que somos a escolha certa?</h2>
 
-      <div className={styles.content}>
-        <section className={styles.info}>
-          <div className={styles.iconContainer}>
-            {differentialsArray[selectedDifferential].Icon}
+      <ul className={styles.list}>
+        <li className={`${styles.item} ${styles.left}`}>
+          <TenIcon />
+          <div>
+            <h3>Mais de 10 anos de experiencia</h3>
+            <p>Impactando vidas e negócios</p>
           </div>
-          <p>{differentialsArray[selectedDifferential].text}</p>
-        </section>
-        <ul className={styles.list}>
-          {differentialsArray.map((differential, index) => (
-            <li
-              key={index}
-              onClick={() => {
-                setSelectedDifferential(index);
-              }}
-            >
-              <h3>{differential.title}</h3>
-              <p>{differential.subtitle}</p>
-            </li>
-          ))}
-        </ul>
-      </div>
+        </li>
+
+        <li className={`${styles.item} ${styles.left}`}>
+          <HandIcon />
+          <div>
+            <h3>Melhor preço do mercado</h3>
+            <p>Preços baixos com alta qualidade de serviço</p>
+          </div>
+        </li>
+
+        <li className={`${styles.item} ${styles.left}`}>
+          <CapIcon />
+          <div>
+            <h3>Vasto conhecimento técnico</h3>
+            <p>Apoio dos melhores professores da UFBA</p>
+          </div>
+        </li>
+
+        <li className={`${styles.item} ${styles.left}`}>
+          <CheckIcon />
+          <div>
+            <h3>Selo de qualidade</h3>
+            <p>Quem contrata, recomenda. A nossa média de NPS é 9,5.</p>
+          </div>
+        </li>
+      </ul>
     </article>
   );
 };
