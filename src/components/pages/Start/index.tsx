76bdb722fb464c792105dd03ad942f9830ee30da
Silas Henrique <silash35@gmail.com>
2023-01-26 21:45:18 -0300

    add hover animation on start image

--- src/components/pages/Start/index.tsx
+++ src/components/pages/Start/index.tsx
@@ -22,20 +22,22 @@ const Start = () => {
       </m.div>
 
       <m.div {...fadeInTop} transition={{ duration: 0.4 }} className={styles.image}>
-        <div>
-          <Image src={bedroomOff} alt="Quarto com as luzes apagadas" />
-        </div>
+        <m.div whileHover={{ scale: 1.1 }}>
+          <div>
+            <Image src={bedroomOff} alt="Quarto com as luzes apagadas" />
+          </div>
 
-        <m.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ repeat: Infinity, repeatType: "reverse", delay: 5, repeatDelay: 5 }}
-        >
-          <Image
-            src={bedroomOn}
-            alt="Quarto com as luzes acessas"
-            style={{ position: "absolute" }}
-          />
+          <m.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ repeat: Infinity, repeatType: "reverse", delay: 5, repeatDelay: 5 }}
+          >
+            <Image
+              src={bedroomOn}
+              alt="Quarto com as luzes acessas"
+              style={{ position: "absolute" }}
+            />
+          </m.div>
         </m.div>
       </m.div>
     </Page>
