9d6e276e0f80096d903324e4d2502e6420fc1bdd
Silas Henrique <silash35@gmail.com>
2023-01-27 21:00:40 -0300

    add sticky to service page

--- src/components/common/Card/index.tsx
+++ src/components/common/Card/index.tsx
@@ -4,10 +4,18 @@ import styles from "./card.module.scss";
 
 interface Props {
   layout?: "column" | "row";
+  noAnime?: boolean;
+
   children: React.ReactNode;
 }
 
-const Card = ({ layout = "column", children }: Props) => {
+const Card = ({ layout = "column", noAnime, children }: Props) => {
+  const className = `${styles.card} ${styles[layout]}`;
+
+  if (noAnime) {
+    return <li className={className}>{children}</li>;
+  }
+
   return (
     <m.li
       initial={{ y: 20, opacity: 0, boxShadow: "none" }}
--- src/components/pages/Services/index.tsx
+++ src/components/pages/Services/index.tsx
@@ -1,93 +1,122 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import Button from "@/components/common/Button";
 import Card from "@/components/common/Card";
 import Page from "@/components/common/Page";
 import Title from "@/components/common/Title";
+import Socket from "@/images/socket.png";
 
 import styles from "./services.module.scss";
 
 const Services = () => {
   return (
-    <Page backgroundColor="secondary">
-      <Title>
-        <span>Serviços</span>
-        <h2>Conheça nossas soluções</h2>
-      </Title>
+    <div className={styles.pageContainer}>
+      <div className={styles.sticky}>
+        <Page backgroundColor="secondary" fullHeight className={styles.services}>
+          <Title>
+            <span>Serviços</span>
+            <h2>Conheça as&nbsp;nossas soluções</h2>
+          </Title>
 
-      <ul className={styles.list}>
-        <Card>
-          <h3>Controle de Iluminação</h3>
-          <p>
-            A OPTIMUS Jr. automatiza as luzes da sua casa, tornando possível o controle da
-            iluminação pelo celular ou pela sua assistente virtual.
-          </p>
-        </Card>
+          <ul className={styles.list}>
+            <ServiceCard image={<Image src={Socket} alt="Foto de uma tomada" />}>
+              <h3>Controle de Iluminação</h3>
+              <p>
+                A OPTIMUS Jr. automatiza as luzes da sua casa, tornando possível o controle da
+                iluminação pelo celular ou pela sua assistente virtual.
+              </p>
+            </ServiceCard>
 
-        <Card>
-          <h3>Automação de Tomadas</h3>
-          <p>
-            Com a automatização de tomadas elétricas, você pode controlar os eletrodomésticos da sua
-            casa de qualquer lugar, economizando energia e facilitando sua vida.
-          </p>
-        </Card>
+            <ServiceCard image={<Image src={Socket} alt="Foto de uma tomada" />}>
+              <h3>Automação de Tomadas</h3>
+              <p>
+                Com a automatização de tomadas elétricas, você pode controlar os eletrodomésticos da
+                sua casa de qualquer lugar, economizando energia e facilitando sua vida.
+              </p>
+            </ServiceCard>
 
-        <Card>
-          <h3>Controle de dispositivos</h3>
-          <p>
-            Ajuste a temperatura do Ar Condicionado, controle a Televisão e muito mais por meio do
-            seu celular, por comandos de voz e até mesmo agendando horários para que funcionem
-            sozinhos.
-          </p>
-        </Card>
+            <ServiceCard image={<Image src={Socket} alt="Foto de uma tomada" />}>
+              <h3>Controle de dispositivos</h3>
+              <p>
+                Ajuste a temperatura do Ar Condicionado, controle a Televisão e muito mais por meio
+                do seu celular, por comandos de voz e até mesmo agendando horários para que
+                funcionem sozinhos.
+              </p>
+            </ServiceCard>
 
-        <Card>
-          <h3>Automação de Cortinas</h3>
-          <p>
-            Transformamos sua casa em uma verdadeira Smart Home! Automatize suas cortinas e tenha
-            controle total sobre o nível de luz e a privacidade da sua residência.{" "}
-          </p>
-        </Card>
+            <ServiceCard image={<Image src={Socket} alt="Foto de uma tomada" />}>
+              <h3>Automação de Cortinas</h3>
+              <p>
+                Transformamos sua casa em uma verdadeira Smart Home! Automatize suas cortinas e
+                tenha controle total sobre o nível de luz e a privacidade da sua residência.
+              </p>
+            </ServiceCard>
 
-        <Card>
-          <h3>Instalação de Fechadura digital</h3>
-          <p>
-            Dê adeus as chaves e aumente a segurança da sua casa tendo a conveniência de abrir e
-            fechar a porta usando seu smartphone, senha, biometria ou cartão.
-          </p>
-        </Card>
+            <ServiceCard image={<Image src={Socket} alt="Foto de uma tomada" />}>
+              <h3>Instalação de Fechadura digital</h3>
+              <p>
+                Dê adeus as chaves e aumente a segurança da sua casa tendo a conveniência de abrir e
+                fechar a porta usando seu smartphone, senha, biometria ou cartão.
+              </p>
+            </ServiceCard>
 
-        <Card>
-          <h3>Automação de Portão Eletrônico</h3>
-          <p>
-            Controle a garagem da sua casa de qualquer lugar pelo seu celular automatizando seu
-            portão.
-          </p>
-        </Card>
+            <ServiceCard image={<Image src={Socket} alt="Foto de uma tomada" />}>
+              <h3>Automação de Portão Eletrônico</h3>
+              <p>
+                Controle a garagem da sua casa de qualquer lugar pelo seu celular automatizando seu
+                portão.
+              </p>
+            </ServiceCard>
 
-        <Card>
-          <h3>Controle de Piscinas</h3>
-          <p>
-            Nunca mais tenha que se preocupar com a bomba da sua piscina. Nós automatizamos seu
-            funcionamento para que ela sempre circule a água sozinha no horário desejado por você.
-          </p>
-        </Card>
+            <ServiceCard image={<Image src={Socket} alt="Foto de uma tomada" />}>
+              <h3>Controle de Piscinas</h3>
+              <p>
+                Nunca mais tenha que se preocupar com a bomba da sua piscina. Nós automatizamos seu
+                funcionamento para que ela sempre circule a água sozinha no horário desejado por
+                você.
+              </p>
+            </ServiceCard>
 
-        <Card>
-          <h3>Instalação de Sensores</h3>
-          <p>
-            Acione dispositivos automaticamente com sensores de presença, umidade, temperatura e
-            muito mais, trazendo conforto, segurança e economia de energia para sua casa.
-          </p>
-        </Card>
+            <ServiceCard image={<Image src={Socket} alt="Foto de uma tomada" />}>
+              <h3>Instalação de Sensores</h3>
+              <p>
+                Acione dispositivos automaticamente com sensores de presença, umidade, temperatura e
+                muito mais, trazendo conforto, segurança e economia de energia para sua casa.
+              </p>
+            </ServiceCard>
 
-        <Card>
-          <h3>Criação de Cenários</h3>
-          <p>
-            Integre todas as automações anteriores e controle diferentes dispositivos ao mesmo tempo
-            com a criação de cenas de modo a criar uma experiência ainda mais personalizada e
-            conveniente em sua casa inteligente.
-          </p>
-        </Card>
-      </ul>
-    </Page>
+            <ServiceCard image={<Image src={Socket} alt="Foto de uma tomada" />}>
+              <h3>Criação de Cenários</h3>
+              <p>
+                Integre todas as automações anteriores e controle diferentes dispositivos ao mesmo
+                tempo com a criação de cenas de modo a criar uma experiência ainda mais
+                personalizada e conveniente em sua casa inteligente.
+              </p>
+            </ServiceCard>
+          </ul>
+        </Page>
+      </div>
+    </div>
+  );
+};
+
+interface Props {
+  image: React.ReactNode;
+  children: React.ReactNode;
+}
+
+const ServiceCard = ({ image, children }: Props) => {
+  return (
+    <Card noAnime>
+      {image}
+      <div className={styles.content}>
+        {children}
+        <Button as={Link} href="/budget">
+          Faça um Orçamento
+        </Button>
+      </div>
+    </Card>
   );
 };
 
