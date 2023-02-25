1ce9c5f5eaa7659e028664704acdabf19751b9cd
Silas Henrique <silash35@gmail.com>
2023-02-25 15:08:12 -0300

    add Google Tag Manager

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,10 @@ import { League_Spartan } from "@next/font/google";
 import { domAnimation, LazyMotion } from "framer-motion";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import Script from "next/script";
 
 import Layout from "@/components/Layout";
+
 const league = League_Spartan({
   subsets: ["latin"],
   fallback: ["Roboto", "sans-serif"],
@@ -26,6 +28,16 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
           <Component {...pageProps} />
         </Layout>
       </LazyMotion>
+
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-NJZG4JZ');
+      `}
+      </Script>
     </>
   );
 };
