44a50cf0707cb15dae2513746362097acfb8045d
silash35 <silash35@gmail.com>
2023-04-14 23:59:47 -0300

    finish prosel

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,6 @@ export default {
   WHATSAPP: "+55 71 9610-0916", // Não esquecer do código do país (+55)
   BUDGET_URL: "https://wa.me/557196100916",
 
-  IS_PROSEL: true,
+  IS_PROSEL: false,
   PROSEL_FORM_URL: "https://forms.gle/YYviJ7yPabRXaSZn8",
 };
