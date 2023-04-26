91d329dec19c53832eaba3ee8f394099db309c54
Silas Henrique <silash35@gmail.com>
2023-04-26 13:07:38 -0300

    fix config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,8 @@ export default {
   EMAIL: "contato@optimusjr.com.br",
   PHONE_NUMBER: "(71) 99610-0916",
   WHATSAPP: "+55 71 9610-0916", // Não esquecer do código do país (+55)
-  BUDGET_URL: "https://wa.me/557196100916?text=Ol%C3%A1%2C%20gostaria%20de%20solicitar%20um%20or%C3%A7amento.",
+  BUDGET_URL:
+    "https://wa.me/557196100916?text=Ol%C3%A1%2C%20gostaria%20de%20solicitar%20um%20or%C3%A7amento.",
   IS_PROSEL: false,
   PROSEL_FORM_URL: "https://forms.gle/YYviJ7yPabRXaSZn8",
 };
