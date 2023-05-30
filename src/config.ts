1fe9b15aa3351a60892555b4f0bcd5157514f28c
TiagoLael <91750773+TiagoLael@users.noreply.github.com>
2023-05-30 14:36:06 -0300

    Atualizar link de orçamento

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,7 @@ export default {
   EMAIL: "contato@optimusjr.com.br",
   PHONE_NUMBER: "(71) 99610-0916",
   WHATSAPP: "+55 71 9610-0916", // Não esquecer do código do país (+55)
-  BUDGET_URL:
-    "https://wa.me/557196100916?text=Ol%C3%A1%2C%20gostaria%20de%20solicitar%20um%20or%C3%A7amento.",
+  BUDGET_URL: "https://form.jotform.com/230868301554052",
   IS_PROSEL: false,
   PROSEL_FORM_URL: "https://forms.gle/YYviJ7yPabRXaSZn8",
 };
