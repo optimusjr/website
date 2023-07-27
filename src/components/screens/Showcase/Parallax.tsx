a316c2ebd3669d0db21c7aa333a0e45cc239a51b
Silas Henrique <silash35@gmail.com>
2023-07-27 13:29:17 -0300

    add hoverFactor on showcase

--- src/components/screens/Showcase/Parallax.tsx
+++ src/components/screens/Showcase/Parallax.tsx
@@ -25,7 +25,11 @@ const Parallax = ({ children, baseVelocity }: ParallaxProps) => {
   const baseX = useMotionValue(0);
   const { scrollY } = useScroll();
   const scrollVelocity = useVelocity(scrollY);
-  const smoothVelocity = useSpring(scrollVelocity, { damping: 15, mass: 0.27, stiffness: 55 });
+  const smoothVelocity = useSpring(scrollVelocity, {
+    damping: 15,
+    mass: 0.27,
+    stiffness: 55,
+  });
   const velocityFactor = useTransform(smoothVelocity, [0, 1000], [0, 10], {
     clamp: false,
   });
@@ -34,9 +38,12 @@ const Parallax = ({ children, baseVelocity }: ParallaxProps) => {
   // Como atualmente ele está repetido 2 vezes então vai de 0 a -50
   const x = useTransform(baseX, (v) => `${wrap(0, -50, v)}%`);
 
+  // Desacelerar on hover
+  const hoverFactor = useSpring(1, { stiffness: 30 });
+
   const directionFactor = useRef<number>(1);
   useAnimationFrame((t, delta) => {
-    let moveBy = directionFactor.current * baseVelocity * (delta / 1000);
+    let moveBy = directionFactor.current * baseVelocity * (delta / 1000) * hoverFactor.get();
 
     // Muda a direção da animação dependendo do scroll
     if (velocityFactor.get() < 0) {
@@ -45,20 +52,28 @@ const Parallax = ({ children, baseVelocity }: ParallaxProps) => {
       directionFactor.current = 1;
     }
 
-    moveBy += directionFactor.current * moveBy * velocityFactor.get();
+    moveBy += directionFactor.current * moveBy * velocityFactor.get() * hoverFactor.get();
 
     baseX.set(baseX.get() + moveBy);
   });
 
-  /*
-    A quantidade de vezes que se deve repetir o children vai
-    depender do tamanho do conteúdo e da tela.
-    Atualmente ele está sendo repetido duas vezes.
-    Cuidado que isso vai influencia nos valores que vão dentro da função wrap
-  */
   return (
-    <div className={styles.parallaxContainer}>
+    <div
+      onMouseEnter={() => {
+        hoverFactor.set(1 / 4);
+      }}
+      onMouseLeave={() => {
+        hoverFactor.set(1);
+      }}
+      className={styles.parallaxContainer}
+    >
       <m.div className={styles.parallax} style={{ x }}>
+        {/*
+          A quantidade de vezes que se deve repetir o children vai
+          depender do tamanho do conteúdo e da tela.
+          Atualmente ele está sendo repetido duas vezes.
+          Cuidado que isso vai influencia nos valores que vão dentro da função wrap
+        */}
         {children}
         {children}
       </m.div>
