5d596ae05b557f8f929754c6f1835fcdd4ef5b21
Silas Henrique <silash35@gmail.com>
2023-07-28 23:47:55 -0300

    remove unnecessary react fragments

--- src/components/screens/AboutStart/index.tsx
+++ src/components/screens/AboutStart/index.tsx
@@ -15,74 +15,72 @@ import styles from "./about.module.scss";
 
 const AboutStart = () => {
   return (
-    <>
-      <Screen backgroundColor="secondary">
-        <section className={styles.intro}>
-          <div>
-            <span>OPTIMUS Jr.</span>
-            <h1>A Empresa Junior de Controle e Automação.</h1>
-            <p>Trazemos conforto, segurança e economia para os seus ambientes.</p>
+    <Screen backgroundColor="secondary">
+      <section className={styles.intro}>
+        <div>
+          <span>OPTIMUS Jr.</span>
+          <h1>A Empresa Junior de Controle e Automação.</h1>
+          <p>Trazemos conforto, segurança e economia para os seus ambientes.</p>
 
-            <BudgetButton />
-          </div>
+          <BudgetButton />
+        </div>
 
-          <Card className={styles.image}>
-            <Image
-              alt="Logotipo da OPTIMUS Jr."
-              placeholder="empty"
-              src={logo}
-              width={350}
-              priority
-            />
-          </Card>
-        </section>
+        <Card className={styles.image}>
+          <Image
+            alt="Logotipo da OPTIMUS Jr."
+            placeholder="empty"
+            src={logo}
+            width={350}
+            priority
+          />
+        </Card>
+      </section>
 
-        <ul className={styles.list}>
-          <ListCard>
-            <TargetIcon />
+      <ul className={styles.list}>
+        <ListCard>
+          <TargetIcon />
 
-            <Typography variant="h3">Missão</Typography>
-            <Typography>
-              Desenvolver nossos membros na área de automação, através da vivência empresarial, com
-              foco em trazer a melhor experiência para os nossos clientes.
+          <Typography variant="h3">Missão</Typography>
+          <Typography>
+            Desenvolver nossos membros na área de automação, através da vivência empresarial, com
+            foco em trazer a melhor experiência para os nossos clientes.
+          </Typography>
+        </ListCard>
+
+        <ListCard>
+          <DiamondIcon />
+
+          <Typography variant="h3">Valores</Typography>
+          <ul className={styles.values}>
+            <Typography component="li" variant="p">
+              <CheckOutlineIcon /> Autonomia
             </Typography>
-          </ListCard>
-
-          <ListCard>
-            <DiamondIcon />
-
-            <Typography variant="h3">Valores</Typography>
-            <ul className={styles.values}>
-              <Typography component="li" variant="p">
-                <CheckOutlineIcon /> Autonomia
-              </Typography>
-              <Typography component="li" variant="p">
-                <CheckOutlineIcon /> Resiliência
-              </Typography>
-              <Typography component="li" variant="p">
-                <CheckOutlineIcon /> Comprometimento com resultados
-              </Typography>
-              <Typography component="li" variant="p">
-                <CheckOutlineIcon /> Sentimento de Ser Dono
-              </Typography>
-              <Typography component="li" variant="p">
-                <CheckOutlineIcon /> Orgulho de ser OPTIMUS
-              </Typography>
-            </ul>
-          </ListCard>
-
-          <ListCard>
-            <EyeIcon />
-
-            <Typography variant="h3">Visão</Typography>
-            <Typography>
-              Entrar e atuar com constância no mercado de automação residencial, com preços justos e
-              qualidade de excelência, gerando conforto, praticidade e economia para os clientes.
+            <Typography component="li" variant="p">
+              <CheckOutlineIcon /> Resiliência
             </Typography>
-          </ListCard>
-        </ul>
-      </Screen>
-    </>
+            <Typography component="li" variant="p">
+              <CheckOutlineIcon /> Comprometimento com resultados
+            </Typography>
+            <Typography component="li" variant="p">
+              <CheckOutlineIcon /> Sentimento de Ser Dono
+            </Typography>
+            <Typography component="li" variant="p">
+              <CheckOutlineIcon /> Orgulho de ser OPTIMUS
+            </Typography>
+          </ul>
+        </ListCard>
+
+        <ListCard>
+          <EyeIcon />
+
+          <Typography variant="h3">Visão</Typography>
+          <Typography>
+            Entrar e atuar com constância no mercado de automação residencial, com preços justos e
+            qualidade de excelência, gerando conforto, praticidade e economia para os clientes.
+          </Typography>
+        </ListCard>
+      </ul>
+    </Screen>
   );
 };
 
--- src/components/screens/LightsPageStart/index.tsx
+++ src/components/screens/LightsPageStart/index.tsx
@@ -10,53 +10,51 @@ import styles from "./lights.module.scss";
 
 const LightsStart = () => {
   return (
-    <>
-      <Screen backgroundColor="secondary">
-        <section className={styles.intro}>
-          <span>Automação de luzes</span>
-          <h1>
-            Controle suas luzes de
-            <br />
-            qualquer lugar
-          </h1>
-          <p>Economize energia e torne sua casa mais conectada</p>
+    <Screen backgroundColor="secondary">
+      <section className={styles.intro}>
+        <span>Automação de luzes</span>
+        <h1>
+          Controle suas luzes de
+          <br />
+          qualquer lugar
+        </h1>
+        <p>Economize energia e torne sua casa mais conectada</p>
 
-          <BudgetButton />
-        </section>
+        <BudgetButton />
+      </section>
 
-        <ul className={styles.list}>
-          <ListCard>
-            <SofaIcon />
+      <ul className={styles.list}>
+        <ListCard>
+          <SofaIcon />
 
-            <Typography variant="h3">Tenha o Conforto da Iluminação Inteligente</Typography>
-            <Typography>
-              Controle as luzes da sua casa pelo celular ou por comando de voz, sem precisar sair do
-              sofá.
-            </Typography>
-          </ListCard>
+          <Typography variant="h3">Tenha o Conforto da Iluminação Inteligente</Typography>
+          <Typography>
+            Controle as luzes da sua casa pelo celular ou por comando de voz, sem precisar sair do
+            sofá.
+          </Typography>
+        </ListCard>
 
-          <ListCard>
-            <ShieldHome />
+        <ListCard>
+          <ShieldHome />
 
-            <Typography variant="h3">Simule Presença, Afaste Intrusos</Typography>
-            <Typography>
-              Controle suas lâmpadas remotamente e programe horários e timers para simular presença
-              e afastar intrusos.
-            </Typography>
-          </ListCard>
+          <Typography variant="h3">Simule Presença, Afaste Intrusos</Typography>
+          <Typography>
+            Controle suas lâmpadas remotamente e programe horários e timers para simular presença e
+            afastar intrusos.
+          </Typography>
+        </ListCard>
 
-          <ListCard>
-            <Leaf />
+        <ListCard>
+          <Leaf />
 
-            <Typography variant="h3">Reduza a Conta de Luz com a Automação</Typography>
-            <Typography>
-              Evite desperdício de energia programando horários para acender e apagar as luzes
-              apenas quando necessário.
-            </Typography>
-          </ListCard>
-        </ul>
-      </Screen>
-    </>
+          <Typography variant="h3">Reduza a Conta de Luz com a Automação</Typography>
+          <Typography>
+            Evite desperdício de energia programando horários para acender e apagar as luzes apenas
+            quando necessário.
+          </Typography>
+        </ListCard>
+      </ul>
+    </Screen>
   );
 };
 
