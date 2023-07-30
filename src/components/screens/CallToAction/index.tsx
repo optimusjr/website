3fe88378a465c3c1991236893fcb2fb0dcac5a51
Silas Henrique <silash35@gmail.com>
2023-07-30 20:06:29 -0300

    add CogsPattern

--- src/components/screens/CallToAction/index.tsx
+++ src/components/screens/CallToAction/index.tsx
@@ -1,10 +1,13 @@
 import BudgetButton from "@/components/common/BudgetButton";
 import Screen from "@/components/common/Screen";
+import CogsPattern from "@/components/patterns/Cogs";
 
 import styles from "./callToAction.module.scss";
 
 const CallToAction = () => (
   <Screen backgroundColor="secondary" className={styles.screen}>
+    <CogsPattern />
+
     <h2>O que você está esperando?</h2>
     <p>Descubra como a automação pode tornar sua vida mais confortável e eficiente.</p>
 
