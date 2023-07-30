a96a7324627e372f114b0bb042ac9105d5580c01
Silas Henrique <silash35@gmail.com>
2023-07-30 20:33:26 -0300

    fix chevron

--- src/components/icons/Chevron.tsx
+++ src/components/icons/Chevron.tsx
@@ -6,12 +6,12 @@ interface Props {
 
 const ChevronIcon = ({ open }: Props) => (
   <m.svg
-    animate={{ rotateX: open ? 0 : 180 }}
+    animate={{ rotateX: open ? 180 : 0 }}
     viewBox="0 0 24 24"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <title>{open ? "Expandir" : "retrair"}</title>
-    <path d={"M7.41,15.41L12,10.83L16.59,15.41L18,14L12,8L6,14L7.41,15.41Z"} />
+    <title>{open ? "Retrair" : "Expandir"}</title>
+    <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
   </m.svg>
 );
 
