73acfd417b611b197e16e31d75a21f3698b68c52
Silas Henrique <silash35@gmail.com>
2023-12-27 18:42:38 -0300

    remove /quote from not index

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 
 // Array para colocar as páginas que não devem ser indexadas pelos mecanismos de busca
-const notIndex = ["/thanks", "/404", "/links", "/quote"];
+const notIndex = ["/thanks", "/404", "/links"];
 
 export default {
   siteUrl: process.env.SITE_URL,
