2c48ff12a54c90cf5db6c69e9eee6a5776f5d80b
Silas Henrique <silash35@gmail.com>
2023-12-29 20:51:51 -0300

    update eslint-config-silash35

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,3 @@
 import prettierConfig from "eslint-config-silash35/.prettierrc.js";
 
-prettierConfig.plugins.push("prettier-plugin-tailwindcss");
-prettierConfig.tailwindFunctions = ["tv"];
-
 export default prettierConfig;
