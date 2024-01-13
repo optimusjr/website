7cd1dfb3f667ee4dec5c10de31bcab21c8c06ec9
Silas Henrique <silash35@gmail.com>
2024-01-12 23:09:04 -0300

    animate links decoration and not border

--- src/components/StyledLink.tsx
+++ src/components/StyledLink.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { tv } from "tailwind-variants";
 
 const LinkStyles = tv({
-  base: "max-w-[40vw] overflow-hidden overflow-ellipsis whitespace-nowrap border-b-1 text-lg/tight transition-colors hover:border-transparent",
+  base: "max-w-[40vw] overflow-hidden overflow-ellipsis whitespace-nowrap text-lg/tight underline transition-colors hover:decoration-transparent",
 });
 
 const StyledLink = ({ className, ...props }: React.ComponentProps<typeof Link>) => (
