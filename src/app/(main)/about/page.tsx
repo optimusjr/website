8161674337416319bc6a2c7a5ce7e9c7a89ce848
Silas Henrique <silash35@gmail.com>
2024-02-10 17:49:06 -0300

    Remove partners

--- src/app/(main)/about/page.tsx
+++ src/app/(main)/about/page.tsx
@@ -3,7 +3,6 @@ import { Metadata } from "next";
 import CallToActionScreen from "@/components/others/CallToActionScreen";
 
 import Information from "./_components/Information";
-import Partners from "./_components/Partners";
 import Start from "./_components/Start";
 
 export const metadata: Metadata = {
@@ -14,7 +13,6 @@ const About = () => (
   <>
     <Start />
     <Information />
-    <Partners />
     <CallToActionScreen />
   </>
 );
