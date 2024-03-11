2e1d1f790e483b70d04911a4f59eff990290ff14
optimus-jr <ej.optimus@gmail.com>
2024-03-11 14:13:26 -0300

    PROSEL aberto

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,6 @@ export default {
   PHONE_NUMBER: "(71) 99610-0916",
   WHATSAPP: "+55 71 9610-0916", // Não esquecer do código do país (+55)
   BUDGET_URL: "https://form.jotform.com/230868301554052",
-  IS_PROSEL: false,
+  IS_PROSEL: true,
   PROSEL_FORM_URL: "https://forms.gle/YYviJ7yPabRXaSZn8",
 };
