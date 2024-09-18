bb5ac68dd169741e644a7313277c63b69064a1cb
Italo <italocs.ofc@gmail.com>
2024-09-18 20:45:42 -0300

    tomadas - lp - att

--- src/app/(main)/lp/automacaodetomadas/content.tsx
+++ src/app/(main)/lp/automacaodetomadas/content.tsx
@@ -5,12 +5,12 @@ import ShieldHome from "@/components/icons/ShieldHome";
 import SofaIcon from "@/components/icons/Sofa";
 import CallToActionScreen from "@/components/others/CallToActionScreen";
 import juliaPolena from "@/images/clients/juliaPolena.jpg";
-import img1 from "@/images/lp/luz/luz2.png";
-import img2 from "@/images/lp/luz/luz3.jpg";
-import img3 from "@/images/lp/luz/luz4.jpg";
-import img4 from "@/images/lp/luz/luz6.jpg";
-import img5 from "@/images/lp/luz/luz7.png";
-import img6 from "@/images/lp/luz/luz8.jpg";
+import img1 from "@/images/lp/tomada/img1.png";
+import img2 from "@/images/lp/tomada/img2.jpg";
+import img3 from "@/images/lp/tomada/img3.jpg";
+import img4 from "@/images/lp/tomada/img4.jpg";
+import img5 from "@/images/lp/tomada/img5.png";
+import img6 from "@/images/lp/tomada/img6.jpg";
 
 import { FAQ, FAQQuestion } from "../_components/FAQ";
 import { Gallery, GalleryImage } from "../_components/Gallery";
@@ -75,45 +75,45 @@ const Content = () => (
     </Gallery>
 
     <FAQ>
-      <FAQQuestion question="É necessário quebrar paredes para fazer a automação de lâmpadas?">
+      <FAQQuestion question="O que exatamente é uma automação de tomada? Como funciona isso?">
+        A automação de tomada permite que você controle dispositivos eletrônicos, ligando ou
+        desligando-os remotamente, geralmente por meio de um aplicativo no smartphone. É bem
+        prático!
+      </FAQQuestion>
+
+      <FAQQuestion question="É necessário quebrar paredes para fazer a automação de toamada?">
         De maneira nenhuma. Com a OPTIMUS Jr., não é preciso quebrar paredes para realizar a
-        automação de lâmpadas. Nossas soluções de automação são projetadas para serem instaladas de
+        automação de tomadas. Nossas soluções de automação são projetadas para serem instaladas de
         forma não invasiva, sem a necessidade de qualquer intervenção estrutural em sua casa. Assim,
-        você pode desfrutar dos benefícios da automação de lâmpadas de maneira prática e rápida, sem
+        você pode desfrutar dos benefícios da automação de tomadas de maneira prática e rápida, sem
         se preocupar com reformas ou danos nas paredes.
       </FAQQuestion>
-      <FAQQuestion question="A OPTIMUS Jr. faz automação de lâmpadas em ambientes externos?">
-        Sim, com certeza! Nossos sistemas de automação de lâmpadas são perfeitamente adequados para
-        uso em ambientes externos como jardins, quintais, varandas e áreas de lazer. Na OPTIMUS Jr.,
-        entendemos a importância de proporcionar iluminação inteligente e conveniente tanto dentro
-        quanto fora de sua casa.
+
+      <FAQQuestion question="Quais são as vantagens de usar uma tomada automatizada? Vale a pena?">
+        Com certeza! Você ganha em conveniência, consegue economizar energia e até aumentar a
+        segurança da sua casa, desligando aparelhos que ficam esquecidos. Isso tudo ajuda a otimizar
+        seu dia a dia.
       </FAQQuestion>
-      <FAQQuestion question="Posso automatizar apenas algumas lâmpadas ou é necessário automatizar todas de uma vez?">
-        Você escolhe! Você pode contratar a automação de apenas uma lâmpada ou fazer logo de todas
-        de uma vez, tudo de acordo com suas necessidades e orçamento.
+
+      <FAQQuestion question="Quais dispositivos posso conectar? Tem limite?">
+        Você pode conectar uma variedade de aparelhos, como lâmpadas, ventiladores e até
+        eletrodomésticos. Contanto que seja conferido a capacidade de carga da tomada para garantir
+        que está tudo certo.
       </FAQQuestion>
-      <FAQQuestion question="Eu preciso ter uma Alexa para automatizar minhas lâmpadas?">
-        Não, você não precisa ter uma Alexa para automatizar suas lâmpadas. Apesar de ser mais legal
-        utilizar o comando de voz pela Alexa, também é possível utilizar apenas o aplicativo para
-        controlar as lâmpadas, ou usar o comando de voz por meio do Google Assistant do seu celular.
+
+      <FAQQuestion question="Posso programar horários para ligar ou desligar?">
+        Sim, a maioria das tomadas automáticas permite que você programe horários específicos. Isso
+        é bem fácil de configurar pelo aplicativo.
       </FAQQuestion>
-      <FAQQuestion question="A automação de lâmpadas é compatível com diferentes tipos de lâmpadas, como LED, fluorescentes ou incandescentes?">
-        Sim, nós automatizamos qualquer tipo de lâmpada. O sistema de automação trabalha diretamente
-        no circuito elétrico da casa, permitindo que você automatize qualquer lâmpada conectada a
-        esse circuito.
+      <FAQQuestion question="A automação é compatível com assistentes de voz?">
+        Sim, muitas das soluções que oferecemos são compatíveis com assistentes de voz como Alexa e
+        Google Assistant, permitindo que você controle os dispositivos com comandos de voz.
       </FAQQuestion>
-      <FAQQuestion question="Quero automatizar as lâmpadas, mas manter os interruptores que já tenho em casa. É possível?">
-        Sim! Apesar de termos interruptores inteligentes com design moderno e controle por touch,
-        entendemos que algumas pessoas preferem manter a estética e a familiaridade de seus
-        interruptores convencionais. Por isso, oferecemos soluções de automação que podem ser
-        instaladas de forma discreta por trás dos interruptores existentes, permitindo que você
-        controle suas lâmpadas de maneira automatizada sem afetar o funcionamento dos interruptores
-        que você já possui.
-      </FAQQuestion>
-      <FAQQuestion question="Oque acontece com as automações quando a internet cai?">
-        Quando a conexão com a internet estiver inativa, o controle remoto das lâmpadas através do
-        aplicativo ou assistente de voz pode ser temporariamente afetado. No entanto, o controle
-        manual através dos interruptores físicos continua funcionando normalmente.
+      <FAQQuestion question="É possível usar as tomadas que eu ja tenho em vez de trocar elas?">
+        Com certeza! Nos possuímos diversas soluções que se conectam às tomadas existentes,
+        permitindo o controle remoto dos aparelhos que já estão plugados. Dessa forma, você pode
+        integrar a automação na sua casa sem a necessidade de substituir as tomadas. Nossa equipe
+        pode ajudar a escolher as melhores opções para suas necessidades!
       </FAQQuestion>
     </FAQ>
 
