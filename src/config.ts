ace055c307929b69a3b28c923ef66f266ccefb41
Raphael Carneiro <raphaelcarneiroufba@gmail.com>
2026-04-13 13:23:19 -0300

    Prosel att

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export default {
   SITE_URL: "https://optimusjr.com.br",
   QUOTE_URL: "/quote",
-  IS_PROSEL: true,
+  IS_PROSEL: false,
   PROSEL_FORM_URL: "https://forms.gle/9T7WfMTKbWHPyLGz7",
 
   EMAIL: "contato@optimusjr.com.br",
